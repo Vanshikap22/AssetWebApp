5d538884c29f7ca59a4230f49c674a421575a783
OmkarNikam222 <onikam193@gmail.com>
2025-10-03 13:26:50 -0400

    new build with href change

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3758746037"
+    serviceWorkerVersion: "2990420884"
   }
 });
